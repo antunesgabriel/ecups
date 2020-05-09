123b6066575c2ac515e9b551967f18c35e00b7d4
antunesgabriel <gabrielantunescontato@gmail.com>
2020-05-08 21:16:29 -0300

    fix

--- src/modules/league-thumb/league-thumb.module.ts
+++ src/modules/league-thumb/league-thumb.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { LeagueThumbController } from './league-thumb.controller';
+import { LeagueModule } from '@modules/league/league.module';
 
 @Module({
-  controllers: [LeagueThumbController]
+  controllers: [LeagueThumbController],
+  imports: [LeagueModule],
 })
 export class LeagueThumbModule {}
